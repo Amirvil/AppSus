3229f803c23f2c0f12d8b0ba79faf580db418b9b
Amirvil <amir.vilensky@gmail.com>
2026-03-12 11:08:28 +0200

    Design the text input for adding note

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -3,13 +3,26 @@ import { NotePreview } from './NotePreview.jsx'
 
 export function NoteList({ notes }) {
     return <div className="container">
+
+        <section className="note-composer">
+
+            <form className="note-form">
+                <input className="title-input" type="text" placeholder="Title" />
+                <input className="content-input" type="text" placeholder="Take a note..." />
+            </form>
+
+        </section>
+
         <section className="note-list">
+
             <ul className="note-grid">
                 {notes.map(note => <li className="note-card" key={note.id}>
                     <NotePreview note={note} />
                 </li>)}
             </ul>
+
         </section>
+
     </div>
 
 }
